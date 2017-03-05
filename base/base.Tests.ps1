ea27f4caff2b58db504dee1e99fe83e3b0e232ea
Chris Borckholder <chris.borckholder@zuehlke.com>
2017-03-05 01:12:40 +0100

    Set Env:TOOLS in base module

--- base/base.Tests.ps1
+++ base/base.Tests.ps1
@@ -1,6 +1,9 @@
-Describe "module::base" {
+Describe "Module::Base" {
     It "sets Env:HOME" {
         $Env:HOME | Should Be "$package\home"
+    }
+    It "sets Env:TOOLS" {
+        $Env:TOOLS | Should Be "$package\tools"
     }
 
     It "creates mount scripts" {
--- base/env.partial.ps1
+++ base/env.partial.ps1
@@ -1,2 +1,3 @@
 # setup devpack environment
 $Env:HOME = "$(Join-Path $PSScriptRoot home)"
+$Env:TOOLS = "$(Join-Path $PSScriptRoot tools)"
