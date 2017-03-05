8dd7687c7c61f0e5f7a6979b21d7b91f7eba3148
Borckholder <Chris.Borckholder@zuehlke.com>
2017-03-05 14:29:11 +0100

    Add test for .gitconfig to git module

--- modules/git/git.Tests.ps1
+++ modules/git/git.Tests.ps1
@@ -2,4 +2,7 @@ Describe "module::Git" {
     It "installs git" {
         git --version | Should BeLike "git version 2.12.0*"
     }
+    It "creates a .gitconfig" {
+        "$package\home\.gitconfig" | Should Exist
+    }
 }
