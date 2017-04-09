ae4d1033aed7b4758dc8c96664fc3853dde94afa
Chris Borckholder <chris.borckholder@zuehlke.com>
2017-04-09 21:46:29 +0200

    Check if executable present in vagrant module test

--- modules/vagrant/vagrant.Tests.ps1
+++ modules/vagrant/vagrant.Tests.ps1
@@ -1,5 +1,6 @@
 ﻿Describe "module::Vagrant" {
     It "installs vagrant" {
+        "$package\tools\vagrant\bin\vagrant.exe" | Should Exist
         vagrant --version | Should Be "Vagrant 1.9.2"
     }
 }
